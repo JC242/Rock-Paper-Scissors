62d6c65d10b86829e5898d9110d64c31270133a1
Juan <juanc.herrera@cetys.edu.mx>
2023-03-31 15:38:39 -0700

    Reworking the code

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-const rockpaper = playerAction =>{
-    const actions = ["rock","paper","scissors"];
-    let computerAction = actions[Math.floor(Math.random() *(3 - 0) + 0)]
+const game = (playerAction,computerAction) =>{
+   
     let result = ""
     if(playerAction == computerAction){
         result = "Tie";
         
     }else if(playerAction == "rock" && computerAction == "scissors"){
-        result = "Player Wins";
+        result = "Player Wins, " + playerAction + " beats " + computerAction;
         
     }else if(playerAction == "paper" && computerAction == "rock"){
-        result = "Player Wins";
+        result = "Player Wins, " + playerAction + " beats " + computerAction;
     }
     else if(playerAction == "scissors" && computerAction == "paper"){
-        result = "Player Wins";
+        result = "Player Wins, " + playerAction + " beats " + computerAction;
     }else{
-        result = "Computer Wins";
+        result = "Computer Wins, " + computerAction + " beats " + playerAction;
     }
     return result;
 }
 
-playerAction = prompt("Enter rock, paper o scissors", "rock");
-console.log(rockpaper("scissors"));
+const playRound = times =>{
+    for (let i = 1; i <= times; i++){
+        console.log(game(getPlayerChoice(), getComputerChoice()));
+    }
+}
+const getComputerChoice = () =>{
+    const actions = ["rock","paper","scissors"];
+    let computerAction = actions[Math.floor(Math.random() *(3 - 0) + 0)]
+    return computerAction;
+}
+const getPlayerChoice = () =>{
+    playerAction = prompt("Enter rock, paper o scissors", "rock");
+    playerAction = playerAction.toLowerCase();
+    return playerAction;
+}
+
+playRound(5);
 
 
