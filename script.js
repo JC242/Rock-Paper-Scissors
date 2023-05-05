0bf4978559e3be7f73e9ba1a72ba5514d1c769d5
Juan <juanc.herrera@cetys.edu.mx>
2023-05-05 12:08:28 -0700

    Version .1

--- script.js
+++ script.js
@@ -20,25 +20,29 @@ const playRound = times =>{
     scorePlayer = 0;
     scoreComputer = 0;
     finish = false;
-    while(finish == false){
-        let result = game(getPlayerChoice(), getComputerChoice());
-        if(result == "Player"){
-            scorePlayer++;
-        }else if(result == "Computer"){
-            scoreComputer++;
-        }
-        if(scorePlayer == times || scoreComputer == times){
-            finish = true;
-        } 
-        console.log("Player score: " + scorePlayer + " Computer score: " +scoreComputer)
-    }
-    if(scorePlayer > scoreComputer){
-        console.log("Player wins with: " + scorePlayer);
-    }else if(scorePlayer < scoreComputer){
-        console.log("Computer wins with: " + scoreComputer);
-    }else{
-        console.log("Tie, Player score: " + scorePlayer + " Computer score: " +scoreComputer)
-    }
+   start.classList.add('hidden');
+   actions.classList.replace('hidden','visible');
+   score.classList.replace('hidden','visible'); 
+
+    // while(finish == false){
+    //     let result = game(getPlayerChoice(), getComputerChoice());
+    //     if(result == "Player"){
+    //         scorePlayer++;
+    //     }else if(result == "Computer"){
+    //         scoreComputer++;
+    //     }
+    //     if(scorePlayer == times || scoreComputer == times){
+    //         finish = true;
+    //     } 
+    //     console.log("Player score: " + scorePlayer + " Computer score: " +scoreComputer)
+    // }
+    // if(scorePlayer > scoreComputer){
+    //     console.log("Player wins with: " + scorePlayer);
+    // }else if(scorePlayer < scoreComputer){
+    //     console.log("Computer wins with: " + scoreComputer);
+    // }else{
+    //     console.log("Tie, Player score: " + scorePlayer + " Computer score: " +scoreComputer)
+    // }
 }
 const getComputerChoice = () =>{
     const actions = ["rock","paper","scissors"];
@@ -54,6 +58,15 @@ const getRandomNumber  = (min,max)=>{
     number = Math.floor(Math.random() * (max - min) + min);
     return number;
 } 
-playRound(5);
+//playRound(5);
+
+let start = document.querySelector('.playButton');
+let actions = document.querySelector('.actions');
+let score = document.querySelector('.score');
+
+start.addEventListener('click', function(event){
+    playRound(5)
+});
 
 
+ 
